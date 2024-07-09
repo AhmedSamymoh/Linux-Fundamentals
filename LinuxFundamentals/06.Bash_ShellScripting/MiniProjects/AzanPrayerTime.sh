bb35b2687f3f148bb5d363a3225290c42584a03d
AhmedSamymoh <ahmeds.elnozahy@gmail.com>
2024-07-09 11:44:59 +0300

    Update Azan MiniProject

--- LinuxFundamentals/06.Bash_ShellScripting/MiniProjects/AzanPrayerTime.sh
+++ LinuxFundamentals/06.Bash_ShellScripting/MiniProjects/AzanPrayerTime.sh
@@ -14,20 +14,34 @@ MAGHRIB=$(echo $PRAYER_TIMES | jq -r '.data.timings.Maghrib')
 ISHA=$(echo $PRAYER_TIMES | jq -r '.data.timings.Isha')
 
 # Print the extracted prayer times
-echo "Fajr: $FAJR"
-echo "Dhuhr: $DHUHR"
-echo "Asr: $ASR"
-echo "Maghrib: $MAGHRIB"
-echo "Isha: $ISHA"
+echo "------------------------"
+echo "----- {$DATE} -----"
+echo "------------------------"
+echo "--    Fajr: $FAJR    ---"
+echo "--    Dhuhr: $DHUHR   ---"
+echo "--    Asr: $ASR     ---"
+echo "--    Maghrib: $MAGHRIB ---"
+echo "--    Isha: $ISHA    ---"
+echo "------------------------"
+
 
 # Path to the Azan audio file (replace with your actual path)
-Azan_FILE="$PWD/azan.mp3"
-echo "$Azan_FILE"
-mpg123 "$Azan_FILE"
+Azan_FILE="$PWD/Azan.mp3"
+##echo "$Azan_FILE"
+#mpg123 "$Azan_FILE"
 
-# # Schedule Azan using at command
-# echo "$Azan_FILE" | at $FAJR
-# echo "$Azan_FILE" | at $DHUHR
-# echo "$Azan_FILE" | at $ASR
-# echo "$Azan_FILE" | at $MAGHRIB
-# echo "$Azan_FILE" | at $ISHA
+# Schedule Azan using at command
+echo "mpg123 \"$Azan_FILE\"" | at $FAJR
+echo "mpg123 \"$Azan_FILE\"" | at $DHUHR
+echo "mpg123 \"$Azan_FILE\"" | at $ASR
+echo "mpg123 \"$Azan_FILE\"" | at $MAGHRIB
+echo "mpg123 \"$Azan_FILE\"" | at $ISHA
+
+# # List all scheduled at jobs
+# atq
+
+# # Remove all scheduled at jobs
+# atq | awk '{print $1}' | xargs atrm
+
+# # Verify that all jobs are removed
+# atq
